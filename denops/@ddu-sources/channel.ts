89318f87a876a2ca1cecf6419513edbae9ceb7f2
kamecha <masakikameyama81@gmail.com>
2023-03-14 14:30:04 +0900

    dduのソースのparamによって未読、購読チャンネルのフィルタリングをした

--- denops/@ddu-sources/channel.ts
+++ denops/@ddu-sources/channel.ts
@@ -1,13 +1,11 @@
-import {
-	BaseSource,
-	Item,
-} from "https://deno.land/x/ddu_vim@v1.13.0/types.ts";
+import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v1.13.0/types.ts";
 import type { Denops } from "https://deno.land/x/ddu_vim@v1.13.0/deps.ts";
 import { ActionData } from "../@ddu-kinds/channel.ts";
-import { channelsRecursive } from "../traqvim/model.ts";
+import { channelsRecursive, getSubscribedChannels, getUnreadChannels } from "../traqvim/model.ts";
+import { Channel } from "../traqvim/type.d.ts";
 
 type Params = {
-	// ...
+	type: "all" | "subscribed" | "unread";
 };
 
 export class Source extends BaseSource<Params> {
@@ -18,18 +16,34 @@ export class Source extends BaseSource<Params> {
 	}): ReadableStream<Item<ActionData>[]> {
 		return new ReadableStream({
 			async start(controller) {
-				const channels = await channelsRecursive();
-				const items: Item<ActionData>[] = channels.map((channel) => {
-					return {
-						word: channel.path,
-					};
-				});
+				const channels: Channel[] = await channelsRecursive();
+				const subscribedChannels: Channel[] = await getSubscribedChannels();
+				const unreadChannels: Channel[] = await getUnreadChannels();
+				console.log(subscribedChannels);
+				const items: Item<ActionData>[] = channels
+					.filter((channel) => {
+						switch (args.sourceParams.type) {
+							case "all":
+								return true;
+							case "subscribed":
+								return subscribedChannels.some((c) => c.id === channel.id);
+							case "unread":
+								return unreadChannels.some((c) => c.id === channel.id);
+						}
+					})
+					.map((channel) => {
+						return {
+							word: channel.path,
+						};
+					});
 				controller.enqueue(items);
 				controller.close();
 			},
 		});
 	}
 	params(): Params {
-		return {};
+		return {
+			type: "all",
+		};
 	}
 }
