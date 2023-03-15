9901e9f324fb5cf04cc7a3fdd4a556fccb5bb30c
kamecha <masakikameyama81@gmail.com>
2023-03-15 23:43:54 +0900

    ddcにおける依存関係の整理

--- denops/traqvim/deps.ts
+++ denops/traqvim/deps.ts
@@ -7,4 +7,5 @@ export {
 } from "https://deno.land/x/unknownutil@v1.0.0/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v3.3.2/function/mod.ts";
 export * as vars from "https://deno.land/x/denops_std@v4.0.0/variable/mod.ts";
+export * as ddcVim from "https://deno.land/x/ddc_vim@v3.4.0/types.ts";
 export { type Denops } from "https://deno.land/x/denops_std@v1.0.0/mod.ts";
