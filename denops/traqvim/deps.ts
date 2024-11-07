9bc1a6e28e4ec3c100ff293156a7270e3d7fed08
kamecha <masakikameyama81@gmail.com>
2024-11-07 22:41:18 +0900

    update denops v4.0.0 → v6.5.1

--- denops/traqvim/deps.ts
+++ denops/traqvim/deps.ts
@@ -7,11 +7,11 @@ export {
   is,
 } from "https://deno.land/x/unknownutil@v3.18.1/mod.ts";
 export type { Predicate } from "https://deno.land/x/unknownutil@v3.18.1/mod.ts";
-export type { Denops } from "https://deno.land/x/denops_std@v4.0.0/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v4.0.0/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v4.0.0/variable/mod.ts";
-export * as helper from "https://deno.land/x/denops_std@v4.0.0/helper/mod.ts";
-export * as bufname from "https://deno.land/x/denops_std@v4.0.0/bufname/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v6.5.1/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v6.5.1/function/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v6.5.1/variable/mod.ts";
+export * as helper from "https://deno.land/x/denops_std@v6.5.1/helper/mod.ts";
+export * as bufname from "https://deno.land/x/denops_std@v6.5.1/bufname/mod.ts";
 export * as ddcVim from "https://deno.land/x/ddc_vim@v3.4.0/types.ts";
 export * as ddcVimSource from "https://deno.land/x/ddc_vim@v3.4.0/base/source.ts";
 export * as dduVim from "https://deno.land/x/ddu_vim@v1.13.0/types.ts";
